d0318f6f5fb363048ab74354187e437e0d1c8ae8
Eshan Adithaya <87906517+EshanAdithaya@users.noreply.github.com>
2023-12-05 19:10:49 +0530

    modification

--- server.js
+++ server.js
@@ -1,63 +1,27 @@
+// server.js
+
 const express = require('express');
-const mysql = require('mysql2');
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
-// Create a MySQL connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'studentmanagementsystem',
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Serve static files (HTML, CSS, JS)
+app.use(express.static('public'));
+
+// Handle signup POST request
+app.post('/signup', (req, res) => {
+    // Process signup data here (insert into the 'users' table)
+    const userData = req.body;
+    // Implement your database logic to insert userData into the 'users' table
+
+    // Respond with a JSON message
+    res.json({ message: 'Signup successful' });
 });
-
-// Connect to the MySQL database
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL database:', err);
-  } else {
-    console.log('Connected to MySQL database');
-  }
-});
-
-// Define routes to fetch data from your database
-
-// Get all students
-app.get('/api/students', (req, res) => {
-  connection.query('SELECT * FROM students', (error, results) => {
-    if (error) {
-      console.error('Error executing MySQL query:', error);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.json(results);
-    }
-  });
-});
-
-// Get a specific student by ID
-app.get('/api/students/:id', (req, res) => {
-  const studentId = req.params.id;
-  connection.query('SELECT * FROM students WHERE sID = ?', [studentId], (error, results) => {
-    if (error) {
-      console.error('Error executing MySQL query:', error);
-      res.status(500).send('Internal Server Error');
-    } else if (results.length === 0) {
-      res.status(404).send('Student not found');
-    } else {
-      res.json(results[0]);
-    }
-  });
-});
-
-// Similarly, you can define routes for other tables (teachers, attendance, classes) as needed
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
-
-// Handle graceful shutdown
-process.on('SIGINT', () => {
-  connection.end();
-  process.exit();
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
